35bd1e5cb1b29310b780995ea791c53ee627d192
amanasthana1111 <as.asthana.1111@gmail.com>
2026-03-05 11:41:53 +0530

    add useeff

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { motion } from "motion/react";
 
 import Connect from "../components/Connect";
